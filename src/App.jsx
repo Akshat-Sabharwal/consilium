3a1a71b4b1f5722cc62e99b0ef9e7332ac10998c
Akshat Sabharwal <akshatsabharwal35@gmail.com>
2024-01-23 16:06:04 +0530

    Basic CSS variables defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
 
 export const App = () => {
-  return <h1>Advice-Generator</h1>;
+  const [advice, setAdvice] = useState({ id: 0, statement: "" });
+
+  const getAdvice = (e) => {
+    e.preventDefault();
+    fetch("https://api.adviceslip.com/advice")
+      .then((data) => data.json())
+      .then((advice) =>
+        setAdvice({ id: advice.slip.id, statement: advice.slip.advice })
+      );
+  };
+
+  return (
+    <>
+      <span className="blue-circle"></span>
+      <span className="brown-circle"></span>
+      <article className="advice">
+        <h1 className="heading">Advice #{advice.id}</h1>
+        <hr className="separator" />
+        <p className="statement">{advice.statement}</p>
+      </article>
+    </>
+  );
 };
